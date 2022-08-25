e3eeeaab4d972776092d023dcd5cf3120d90cc1c
execom <33149435+croexe@users.noreply.github.com>
2022-08-25 12:42:45 +0200

    MappingProfile added for Client

---
+++ TaskManager.Infrastructure/MappingProfiles/ClientProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Domain.DTOs;
+using TaskManager.Domain.Models;
+
+namespace TaskManager.Infrastructure.MappingProfiles;
+
+public class ClientProfile : Profile
+{
+    public ClientProfile()
+    {
+        CreateMap<ClientDto, Client>()
+            .ForMember(dest => dest.Name,
+            from => from.MapFrom(src => src.Name))
+            .ForMember(dest => dest.Sector,
+            from => from.MapFrom(src => src.Sector))
+            .ForMember(dest => dest.Note,
+            from => from.MapFrom(src => src.Note));
+
+        CreateMap<Client, ClientDto>().ForMember(
+            dest => dest.Id,
+            from => from.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Name,
+            from => from.MapFrom(src => src.Name));
+
+    }
+}
