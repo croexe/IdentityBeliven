7aab4e9d8dac38401d4d3def2c039aae955fec9d
execom <33149435+croexe@users.noreply.github.com>
2022-08-25 20:46:47 +0200

    UserMessages created

---
+++ TaskManager.Domain/Helpers/UserMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskManager.Domain.Helpers;
+
+public static class UserMessages
+{
+    public static readonly string USER_CREATED_SUCCESSFULY = "User created successfuly.";
+    public static readonly string USER_CREATION_UNSUCCESSFUL = "User creation unsuccessful.";
+    public static readonly string USER_ALREADY_EXISTS = "User already exists.";
+}
