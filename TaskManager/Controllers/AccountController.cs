4308622fa102ba653c6485b2e895372b10aa5567
execom <33149435+croexe@users.noreply.github.com>
2022-08-25 20:46:58 +0200

    Register Method added

--- TaskManager/Controllers/AccountController.cs
+++ TaskManager/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using TaskManager.Domain.Helpers;
 using TaskManager.Domain.Models;
 
 namespace TaskManager.Controllers;
@@ -60,5 +61,36 @@ public class AccountController : ControllerBase
             });
         }
         return Unauthorized();
+    }
+
+    [HttpPost]
+    [Route("register")]
+    public async Task<IActionResult> RegisterAdmin([FromBody] RegisterModel model)
+    {
+        var userExists = await _userManager.FindByNameAsync(model.Username);
+        if (userExists != null)
+            return StatusCode(StatusCodes.Status500InternalServerError, new { Status = "Error", Message = UserMessages.USER_ALREADY_EXISTS });
+
+        IdentityUser user = new IdentityUser()
+        {
+            Email = model.Email,
+            SecurityStamp = Guid.NewGuid().ToString(),
+            UserName = model.Username
+        };
+        var result = await _userManager.CreateAsync(user, model.Password);
+        if (!result.Succeeded)
+            return StatusCode(StatusCodes.Status500InternalServerError, new { Status = "Error", Message = UserMessages.USER_CREATION_UNSUCCESSFUL });
+
+        if (!await _roleManager.RoleExistsAsync(UserRoles.Manager))
+            await _roleManager.CreateAsync(new IdentityRole(UserRoles.Manager));
+        if (!await _roleManager.RoleExistsAsync(UserRoles.Developer))
+            await _roleManager.CreateAsync(new IdentityRole(UserRoles.Developer));
+
+        if (await _roleManager.RoleExistsAsync(UserRoles.Manager))
+        {
+            await _userManager.AddToRoleAsync(user, UserRoles.Manager);
+        }
+
+        return Ok(new { Status = "Success", Message = UserMessages.USER_CREATED_SUCCESSFULY });
     }
 }
