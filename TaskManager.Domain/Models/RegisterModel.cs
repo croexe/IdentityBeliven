0eee49e9a6c36defed97d06141e67c9397280cdc
execom <33149435+croexe@users.noreply.github.com>
2022-08-25 20:46:34 +0200

    Register Model adjusted

--- TaskManager.Domain/Models/RegisterModel.cs
+++ TaskManager.Domain/Models/RegisterModel.cs
@@ -7,4 +7,7 @@ public sealed class RegisterModel : LoginModel
     [EmailAddress]
     [Required(ErrorMessage = "Email is required.")]
     public string Email { get; set; }
+
+    [Required(ErrorMessage = "Type of user is required.")]
+    public string Usertype { get; set; }
 }
