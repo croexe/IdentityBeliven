799a892afdf0ca3c0ba4feed425854a14962f7a0
execom <33149435+croexe@users.noreply.github.com>
2022-08-25 12:41:50 +0200

    Testing Client service, works

---
+++ TaskManager.Infrastructure/Repositories/ClientRepository.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Domain.DTOs;
+using TaskManager.Domain.Interfaces;
+using TaskManager.Domain.Models;
+using TaskManager.Infrastructure.Database;
+
+namespace TaskManager.Infrastructure.Repositories;
+
+public class ClientRepository : IClientRepository
+{
+    private readonly TaskDbContext _context;
+    private readonly IMapper _mapper;
+
+    public ClientRepository(TaskDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+    public async Task<ClientDto> AddClient(ClientDto dto)
+    {
+        var client = _mapper.Map<Client>(dto);
+        try
+        {
+            await _context.Clients.AddAsync(client);
+            await _context.SaveChangesAsync();
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+        var dtoReturn = _mapper.Map<ClientDto>(client);
+        return dtoReturn;
+    }
+}
---
+++ TaskManager/Controllers/ClientController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManager.Domain.DTOs;
+using TaskManager.Domain.Interfaces;
+
+namespace TaskManager.Controllers
+{
+    [Authorize("Project Manager")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly IClientRepository repository;
+
+        public ClientController(IClientRepository _repository)
+        {
+            repository = _repository;
+        }
+        [HttpPost]
+        public Task<ClientDto> Add(ClientDto dto)
+        {
+            var kol = repository.AddClient(dto);
+            return kol;
+        }
+    }
+}
