4bb37effdc4248a6aaf252bc1d6de3ab64e93e77
execom <33149435+croexe@users.noreply.github.com>
2022-08-28 00:05:43 +0200

    Entities adjusted for shadow FK mapping

--- TaskManager.Domain/Entities/Client.cs
+++ TaskManager.Domain/Entities/Client.cs
@@ -1,10 +1,12 @@
-﻿namespace TaskManager.Domain.Entities;
+﻿using System.Collections.ObjectModel;
 
-public sealed class Client : Entity
+namespace TaskManager.Domain.Entities;
+
+public class Client : Entity
 {
     public string Name { get; set; }
     public string Sector { get; set; }
     public string Note { get; set; }
 
-    public IEnumerable<Project> Projects { get; set; }
+    public virtual ICollection<Project> Projects { get; set; } = new Collection<Project>();
 }
--- TaskManager.Domain/Entities/Project.cs
+++ TaskManager.Domain/Entities/Project.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TaskManager.Domain.Entities;
 
-public sealed class Project : Entity
+public class Project : Entity
 {
     public string Name { get; set; }
     public int ClientId { get; set; }
+    [NotMapped]
+    public string UserId { get; set; }
 
     public Client Client { get; set; }
     public IdentityUser ProjectManager { get; set; }
-    public IEnumerable<Task> Tasks { get; set; }
+    public virtual ICollection<Task> Tasks { get; set; } = new Collection<Task>();
 }
