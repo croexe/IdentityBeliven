73e7f0d5a625e6f537727d9cc08b249f39fe1d61
execom <33149435+croexe@users.noreply.github.com>
2022-08-28 00:15:03 +0200

    Code clean up on Repositories

--- TaskManager.Infrastructure/Repositories/TaskRepository.cs
+++ TaskManager.Infrastructure/Repositories/TaskRepository.cs
@@ -1,10 +1,5 @@
 ﻿using AutoMapper;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TaskManager.Domain.DTOs;
 using TaskManager.Domain.Interfaces;
 using TaskManager.Infrastructure.Database;
