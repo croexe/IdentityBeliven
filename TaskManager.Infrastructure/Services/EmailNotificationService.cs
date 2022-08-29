c4eab5158ae8ca65a1b96583c9c0e38e4ab52a1e
execom <33149435+croexe@users.noreply.github.com>
2022-08-29 22:21:59 +0200

    EmailNotificationService created

---
+++ TaskManager.Infrastructure/Services/EmailNotificationService.cs
@@ -0,0 +1,31 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+using TaskManager.Domain.Interfaces;
+
+namespace TaskManager.Infrastructure.Services;
+
+public class EmailNotificationService : INotificationService
+{
+    /* Or put them in AppSettings */
+    private const string HOST = "Some host..";
+    private const int PORT = 587;
+    private const string PASSWORD = "password";
+    
+    public void Send(string recipient, string sender, string title, string status)
+    {
+        var email = new MimeMessage();
+        email.From.Add(MailboxAddress.Parse(sender));
+        email.To.Add(MailboxAddress.Parse(recipient));
+        email.Subject = $"Task {title}";
+        email.Body = new TextPart(TextFormat.Html) { Text = $"<i>{title} is {status}.<i>" };
+
+
+        using var smtp = new SmtpClient();
+        smtp.Connect(HOST, PORT, SecureSocketOptions.StartTls);
+        smtp.Authenticate(sender, PASSWORD);
+        smtp.Send(email);
+        smtp.Disconnect(true);
+    }
+}
